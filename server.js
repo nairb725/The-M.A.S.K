b6c82bf7b09f5c894058d86a16e9fb7049d9af23
Infernaton <lubin.pauly@gmail.com>
2026-02-01 16:30:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const io = new Server(server);
 const port = process.env.PORT || 3000;
 
 let connectedPlayers = [];
-let objective = 50;
+let objective = 1500;
 let isGamePlaying = false;
 let pathArchive = "archive/";
 let firstEater = null;
